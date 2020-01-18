ab79a71dfe412bb82d6d3561925d0aad35338fe7
tylergeery <tyler.geery@yahoo.com>
2020-01-17 21:29:54 -0800

    add button to login screen

---
+++ 2d/scripts/signup/login.gd
@@ -0,0 +1,13 @@
+extends Control
+
+# Declare member variables here. Examples:
+var email = ""
+var password = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _unhandled_input(event):
+	pass
